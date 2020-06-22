c065e4c37fa42909dcdd72ce217b262c06aadaec
Vladimir Popkov <bosun0542desu@gmail.com>
2020-06-22 14:55:12 +0300

    web

---
+++ city-register/src/main/java/edu/javacourse/city/domain/PersonRequest.java
@@ -0,0 +1,78 @@
+package edu.javacourse.city.domain;
+
+import java.time.LocalDate;
+
+public class PersonRequest {
+    private String surName;
+    private String gavenName;
+    private String patronymic;
+    private LocalDate dateOfBirth;
+    private Integer streetCode;
+    private String building;
+    private String extension;
+    private String apartment;
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public String getGavenName() {
+        return gavenName;
+    }
+
+    public void setGavenName(String gavenName) {
+        this.gavenName = gavenName;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Integer getStreetCode() {
+        return streetCode;
+    }
+
+    public void setStreetCode(Integer streetCode) {
+        this.streetCode = streetCode;
+    }
+
+    public String getBuilding() {
+        return building;
+    }
+
+    public void setBuilding(String building) {
+        this.building = building;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public String getApartment() {
+        return apartment;
+    }
+
+    public void setApartment(String apartment) {
+        this.apartment = apartment;
+    }
+}
