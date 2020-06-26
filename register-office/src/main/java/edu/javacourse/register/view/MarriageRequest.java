129f7588f76fd29bd6bbce726c70543b3ca7876d
Vladimir Popkov <bosun0542desu@gmail.com>
2020-06-26 21:37:48 +0300

    before Spring

---
+++ register-office/src/main/java/edu/javacourse/register/view/MarriageRequest.java
@@ -0,0 +1,30 @@
+package edu.javacourse.register.view;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class MarriageRequest implements Serializable {
+    private String husbandSurname;
+    private String husbandGivenName;
+    private String husbandPatronymic;
+    private LocalDate husbandDateOfBirth;
+    private String husbandPassportSeria;
+    private String husbandPassportNumber;
+    private LocalDate husbandPassportIssueDate;
+
+    private String wifeSurname;
+    private String wifeGivenName;
+    private String wifePatronymic;
+    private LocalDate wifeDateOfBirth;
+    private String wifePassportSeria;
+    private String wifePassportNumber;
+    private LocalDate wifePassportIssueDate;
+
+    private String marriageCertificateNumber;
+    private LocalDate marriageCertificateDate;
+
+
+
+
+
+}
